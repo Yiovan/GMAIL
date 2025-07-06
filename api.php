a72f6917600cd3ff91a7bf4f2abf3aa5eda341d3
Giovanni <giovannicabrerarivas@icloud.com>
2025-07-06 17:55:21 -0300

    proyecto final

---
+++ api.php
@@ -0,0 +1,36 @@
+<?php
+$name = $_POST["name"];
+$email = $_POST["email"];
+$subject = $_POST["subject"];
+$message = $_POST["message"];
+
+require "vendor/autoload.php";
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;       
+
+$mail = new PHPMailer(exceptions: true);
+
+//$mail->SMTPDebug = SMTP::DEBUG_SERVER; // Disable debug output
+
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+
+$mail->Host = 'smtp.gmail.com';
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = 587; 
+
+$mail->Username = "giovannicabrerarivas@gmail.com";
+$mail->Password = "bqxh suom gtei hwkd";
+
+$mail->setFrom($email , $name);
+$mail->addAddress('giovannicabrerarivas@gmail.com', 'Formulario de contacto');
+$mail->addReplyTo($email, $name);
+$mail->Subject = $subject;
+$mail->Body = $message;
+
+$mail->send();
+
+header("Location: sent.html");
+
+
